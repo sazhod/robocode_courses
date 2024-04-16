dc391fca54347766e7d8ed032ab69550f68339eb
“sazhod” <sazhod@yandex.ru>
2024-04-16 09:06:10 +0500

    Add method is_moderator_or_methodist

--- backend/core/common/permissions.py
+++ backend/core/common/permissions.py
@@ -13,3 +13,7 @@ class IsMethodist(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (request.user.role == settings.METHODIST or request.user.is_superuser)
 
+
+def is_moderator_or_methodist(request, view):
+    return IsModerator().has_permission(request, view) or IsMethodist().has_permission(request, view)
+
